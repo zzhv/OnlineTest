fe04d1cdbc9f41ffc228498798b1a8c49d08a054
没头脑 <1181836185@qq.com>
2019-03-24 22:55:58 +0800

    增加后台管理界面

---
+++ HOPU/Controllers/SysManageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HOPU.Controllers
+{
+    public class SysManageController : Controller
+    {
+        // GET: SysManage
+        public ActionResult SysManageIndex()
+        {
+            return View();
+        }
+    }
+}
